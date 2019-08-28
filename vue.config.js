7294bf3708a9e08ae5c56232619cee1429a7c861
peterhpchen <peterhsinpingchen@gmail.com>
2019-08-28 17:34:29 +0800

    Add travis config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-dojo/' : '/',
   configureWebpack: {
     plugins: [
       new MonacoWebpackPlugin({
